f1de608cfaeaef66823ddcc7ede5075facdfec73
Jacob Ross <survivaldawn@gmail.com>
2021-02-02 06:28:04 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,33 @@
-
-import './App.css';
+import React, {useState, useEffect} from 'react';
+import Article from './components/Articles';
 
 function App() {
-  return (
+  const [articles, setArticles] = useState([]);
+  const [subreddit, setSubreddit] = useState('wallstreetbbets');
+
+  useEffect(() => {
+    fetch("https://www.reddit.com/r/" + subreddit + ".json")
+      .then(res => {
+        if(res.status != 200) {
+          console.log("ERROR");
+          return;
+        }
+        res.json().then(data => {
+          if(data != null) {
+            setArticles(data.data.children);
+          }
+        })
+      })
+  }, [subreddit]);
+
+  return ( 
     <div className="App">
       <header className="App-header">
-        
-       
-     
+        <input type="text" className="input" value={subreddit} onChange={e => setSubreddit(e.target.value)}></input>
       </header>
+      <div className="articles">
+        {(articles != null ? articles.map((article, index) => <Article key={index} article={article.data}/>) : '')}
+      </div>
     </div>
   );
 }
